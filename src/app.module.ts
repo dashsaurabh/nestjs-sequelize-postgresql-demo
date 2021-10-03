5803a45a79f027fda9c7ffe16aa37945032fbf7b
Saurabh Dashora <saurabhdashora@Saurabhs-MacBook-Air.local>
2021-10-03 09:30:03 +0530

    First Commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LibraryModule } from './library/library.module';
+
+@Module({
+  imports: [SequelizeModule.forRoot({
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'password',
+    database: 'library',
+    autoLoadModels: true,
+    synchronize: true
+  }), LibraryModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
